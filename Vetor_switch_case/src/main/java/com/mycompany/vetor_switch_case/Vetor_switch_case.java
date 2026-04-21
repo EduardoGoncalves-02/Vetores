285996f93b7a37da0167c154adcc11d322c64fe3
EduardoGoncalves-02 <goncalvespereira02edu@gmail.com>
2026-04-20 21:18:04 -0300

    Começo do exercício, 'A' e 'B' já concluídos.

---
+++ Vetor_switch_case/src/main/java/com/mycompany/vetor_switch_case/Vetor_switch_case.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.vetor_switch_case;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adri
+ */
+public class Vetor_switch_case {
+
+    public static void main(String[] args) {
+        int vetor[] = new int[100];
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Quantida de números do vetor: "));
+        for (int i = 0; i < n; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog((i + 1) + ". Número: "));
+        }
+        String opcao = JOptionPane.showInputDialog("""
+                                                   a) Verificar se um determinado valor existe no vetor.
+                                                   b) Informar a posição de um determinado valor no vetor.
+                                                   c) Apresentar a quantidade de ocorrências de um valor específico no vetor.
+                                                   d) Identificar o menor valor do vetor e sua respectiva posição.
+                                                   e) Identificar o maior valor do vetor e sua respectiva posição.
+                                                   f) Determinar o valor central do vetor. Caso N seja par, considerar o primeiro elemento da metade esquerda.
+                                                   g) Calcular o produto de todos os valores do vetor.
+                                                   h) Calcular a soma de todos os valores positivos do vetor.
+                                                   i) Calcular a soma de todos os valores negativos do vetor.
+                                                   j) Calcular a soma de todos os valores pares do vetor.
+                                                   k) Calcular a soma de todos os valores ímpares do vetor.
+                                                   l) Calcular a soma de todos os valores do vetor.
+                                                   m) Calcular a média aritmética dos valores do vetor.
+                                                   n) Exibir os valores do vetor que são maiores que a média.
+                                                   o) Mostrar os valores do vetor em ordem inversa.
+                                                   p) Ordenar o vetor em ordem crescente e exibir o resultado.
+                                                   """);
+        char opcaoCHAR = opcao.charAt(0);
+
+        switch (opcaoCHAR) {
+            case 'a': {
+                int existe = Integer.parseInt(JOptionPane.showInputDialog("Qual valor gostaria de procurar no vetor? "));
+                boolean encontrado = false;
+
+                for (int i = 0; i < n; i++) {
+                    if (existe == vetor[i]) {
+                        encontrado = true;
+                        break;
+                    }
+                }
+
+                if (encontrado) {
+                    JOptionPane.showMessageDialog(null, existe + " existe no vetor.");
+                } else {
+                    JOptionPane.showMessageDialog(null, existe + " NÃO existe no vetor.");
+                }
+                break;
+            }
+            case 'b': {
+                int existe = Integer.parseInt(JOptionPane.showInputDialog("Qual o valor a ser encontrado? "));
+                boolean encontrado = false;
+                int posicao = 0;
+
+                for (int i = 0; i < n; i++) {
+                    if (existe == vetor[i]) {
+                        posicao = posicao + 1;
+                        encontrado = true;
+                        break;
+                    }else{
+                        posicao = posicao + 1;
+                    }
+                    
+                }
+                if (encontrado == true){
+                    JOptionPane.showMessageDialog(null, "O valor " + existe + ", está na posição: " + posicao);
+                }else{
+                    JOptionPane.showMessageDialog(null, "Valor não encontrado.");
+                }
+            }
+        }
+    }
+}
